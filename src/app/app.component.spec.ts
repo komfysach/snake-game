248f73a687d417f6f0a195047c16ea00503b7b4e
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-04-18 19:48:39 +0200

    update app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,41 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FoodService } from './services/food/food.service';
+import { SnakeService } from './services/snake/snake.service';
+import { GameBoardComponent } from './components/game-board/game-board.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let snakeService: SnakeService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [AppComponent, GameBoardComponent],
+      providers: [FoodService, SnakeService],
     }).compileComponents();
+
+    const fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    snakeService = TestBed.inject(SnakeService);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have the 'snake-game' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('snake-game');
+    expect(component.title).toEqual('snake-game');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, snake-game');
+  it('should change direction', () => {
+    const direction = { x: 1, y: 0 };
+    component.changeDirection(direction);
+    expect(snakeService.snake.direction).toEqual(direction);
+  });
+
+  it('should toggle pause', () => {
+    const initialPauseState = snakeService.paused;
+    component.togglePause();
+    expect(snakeService.paused).toBe(!initialPauseState);
   });
 });
