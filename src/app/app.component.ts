448e90679891b38d14685dabae81cc7f47c0fc07
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2024-04-18 19:48:52 +0200

    add mobile controls to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FoodService } from './services/food/food.service';
+import { SnakeService } from './services/snake/snake.service';
+
+type Direction = { x: number; y: number };
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'snake-game';
+  constructor(
+    public foodService: FoodService,
+    private snakeService: SnakeService
+  ) {}
+
+  // for mobile touch devices
+  changeDirection(direction: Direction) {
+    if (this.snakeService.switchControls) {
+      this.snakeService.snake.direction = { x: -direction.x, y: -direction.y };
+    } else {
+      this.snakeService.snake.direction = direction;
+    }
+  }
+
+  togglePause() {
+    this.snakeService.togglePause();
+  }
 }
